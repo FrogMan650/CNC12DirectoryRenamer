494d87dfe136ceb2a1088b13f3dc2cda4977f15b
LucasK650 <lucask650@yahoo.com>
2025-08-23 14:44:23 -0400

    "Done"
    Cleaned everything up, many for loops added to reduce code pretty significantly. Going to consider this "done" for now. It's not pretty but it works.

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -5,12 +6,10 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-import javax.swing.GroupLayout.Alignment;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 import javafx.application.Application;
@@ -19,101 +18,99 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    public static String[] directories = {"cncm", "cnct", "cncr", "cncp", "cncl"};
+    public static String[] machines = {"mill", "lathe", "router", "plasma", "laser"};
     public static void main(String[] args) throws Exception {
         launch(args);
     }
 
+    //method used to change the text next to the buttons
     public static String renamedDirectory(String directory) {
-        return directory + "_" + getVersion(directory) + "_" + getBoardType(directory) + "_" + getMachineType(directory) + "_" + getDate() + "_" + getTime();
+        if (directoryExists(directory)) {
+            String machine;
+            if (directory.equals("cncm") && !getMachineType(directory).equals("mill")) {
+                machine = getMachineType(directory) + "_";
+            } else {
+                machine = "";
+            }
+        return directory + "_" + getVersion(directory) + "_" + getBoardType(directory) + "_" + machine + "_" + getDate() + "_" + getTime();
+        } else {
+            return directory + " does not exist or was already renamed";
+        }
     }
 
+    //method called to actually rename the directory requested
     public static void renameDirectory(String directory) {
-        System.out.println(directory + "_" + getVersion(directory) + "_" + getBoardType(directory) + "_" + getMachineType(directory) + "_" + getDate() + "_" + getTime());
-        // Path sourcePath = Paths.get("C:/" + directory);
-        // Path destinationPath = Paths.get("C:/" + directory + "_" + getVersion(directory) + "_" + getBoardType(directory) + "_" + getMachineType(directory) + "_" + getDate() + "_" + getTime());
-        // try {
-        //     Files.move(sourcePath, destinationPath);
-        // } catch (Exception e) {
-        //     System.out.println("exception thrown while renaming " + directory);
-        //     System.out.println(e);
-        // }
-    }
-
-    public static void renameAllDirectory() {
-        File cncm = new File("C:/cncm");
-        File cnct = new File("C:/cnct");
-        File cncr = new File("C:/cncr");
-        File cncp = new File("C:/cncp");
-        File cncl = new File("C:/cncl");
-        File cncmExe = new File("C:/cncm/cncm.exe");
-        File cnctExe = new File("C:/cncm/cnct.exe");
-        File cncrExe = new File("C:/cncm/cncr.exe");
-        File cncpExe = new File("C:/cncm/cncp.exe");
-        File cnclExe = new File("C:/cncm/cncl.exe");
-        if (cncm.exists()) {
-            if (cncmExe.exists()) {
-                renameDirectory("cncm");
-            } else if (cnctExe.exists()) {
-                renameDirectory("cnct");
-            } else if (cncrExe.exists()) {
-                renameDirectory("cncr");
-            } else if (cncpExe.exists()) {
-                renameDirectory("cncp");
-            } else if (cnclExe.exists()) {
-                renameDirectory("cncl");
+        if (directoryExists(directory)) {
+            String machine;
+            if (directory.equals("cncm") && !getMachineType(directory).equals("mill")) {
+                machine = getMachineType(directory) + "_";
+            } else {
+                machine = "";
+            }
+            Path sourcePath = Paths.get("C:/" + directory);
+            Path destinationPath = Paths.get("C:/" + directory + "_" + getVersion(directory) + "_" + getBoardType(directory) + "_" + machine + getDate() + "_" + getTime());
+            try {
+                Files.move(sourcePath, destinationPath);
+            } catch (Exception e) {
+                System.out.println("exception thrown while renaming " + directory);
+                System.out.println(e);
             }
         }
-        if (cnct.exists()) {
-            renameDirectory("cnct");
-        }
-        if (cncr.exists()) {
-            renameDirectory("cncr");
-        }
-        if (cncp.exists()) {
-            renameDirectory("cncp");
-        }
-        if (cncl.exists()) {
-            renameDirectory("cncl");
+    }
+
+    //method to check if a directory exists
+    public static Boolean directoryExists(String directory) {
+        File file = new File("C:/" + directory);
+        return file.exists();
+    }
+
+    //method to rename a directory only if said directory exists
+    public static void renameIfExists(String directory) {
+        if (directoryExists(directory)) {
+            renameDirectory(directory);
         }
     }
 
+    //method used by allButton to set directory names for any that may exist
+    public static void renameAllDirectory() {
+        for (int i = 0; i < directories.length; i++) {
+            renameIfExists(directories[i]);
+        }
+    }
+
+    //method to return the current date in day-month-year format
     public static String getDate() {
         String dateNow = String.valueOf(LocalDate.now());
         String[] dateSplit = dateNow.split("-");
         return dateSplit[1] + "-" + dateSplit[2] + "-" + dateSplit[0].split("0")[1];
     }
 
+    //method to return the current time in hour.minute format
     public static String getTime() {
         String timeNow = String.valueOf(LocalTime.now());
         String[] timeSplit = timeNow.split(":");
         return timeSplit[0] + "." + timeSplit[1];
     }
 
+    //method to figure out the type of machine based on which .exe is in the directory
     public static String getMachineType(String directory) {
-        File cncmExe = new File("C:/" + directory + "/cncm.exe");
-        File cnctExe = new File("C:/" + directory + "/cnct.exe");
-        File cncrExe = new File("C:/" + directory + "/cncr.exe");
-        File cncpExe = new File("C:/" + directory + "/cncp.exe");
-        File cnclExe = new File("C:/" + directory + "/cncl.exe");
-        if (cncmExe.exists()) {
-            return "mill";
-        } else if (cnctExe.exists()) {
-            return "lathe";
-        } else if (cncrExe.exists()) {
-            return "router";
-        } else if (cncpExe.exists()) {
-            return "plasma";
-        } else if (cnclExe.exists()) {
-            return "laser";
-        } else {
-            return null;
+        for (int i = 0; i < directories.length; i++) {
+            File directoryExe = new File("C:/" + directory + "/" + directories[i] +".exe");
+            if (directoryExe.exists()) {
+                return machines[i];
+            }
         }
+        return null;
     }
 
+    //method to get the directory used for files such as cncm.prm.xml, cnctcfg.xml, etc.
     public static String getFileDirectory(String directory) {
         if (directory.equals("cnct")) {
             return "cnct";
@@ -122,12 +119,13 @@ public class App extends Application {
         }
     }
 
+    //method to get the board type from cnc*/mpu_info.xml
     public static String getBoardType(String directory) {
         NodeList boardVersionNodeList;
         String boardVersion = null;
         String oldFilePath = "C:/" + directory + "/mpu_info.xml";
         try {
-            boardVersionNodeList = getRootElement(getDocument(oldFilePath)).getElementsByTagName("PLCDeviceID");
+            boardVersionNodeList = getDocument(oldFilePath).getDocumentElement().getElementsByTagName("PLCDeviceID");
             boardVersion = boardVersionNodeList.item(0).getTextContent();
         } catch (Exception e) {
             System.out.println("Exception thrown while setting board type");
@@ -136,6 +134,7 @@ public class App extends Application {
         return boardVersion.split("_")[2];
     }
 
+    //method to get a document based on provided path
     public static Document getDocument(String filePath) {
         DocumentBuilderFactory factory;
         DocumentBuilder builder;
@@ -151,40 +150,13 @@ public class App extends Application {
         return document;
     }
 
-    public static Element getRootElement(Document document) {
-        Element rootElement = null;
-        try {
-            rootElement = document.getDocumentElement();
-        } catch (Exception e) {
-            System.out.println("Exception thrown while getting root element from document");
-            System.out.println(e);
-        }
-            return trimEmptyElements(rootElement);
-    }
-
-    public static Element trimEmptyElements(Element node) {
-        for (int i = node.getChildNodes().getLength()-1; i >= 0; i--) {
-            if (node.getChildNodes().item(i).getTextContent().trim().isEmpty() && !node.getChildNodes().item(i).hasAttributes()) {
-                node.removeChild(node.getChildNodes().item(i));
-                continue;
-            }
-            if (node.getChildNodes().item(i).hasChildNodes()) {
-                for (int j = node.getChildNodes().item(i).getChildNodes().getLength()-1; j >= 0; j--) {
-                    if (node.getChildNodes().item(i).getChildNodes().item(j).getTextContent().trim().isEmpty() && !node.getChildNodes().item(i).getChildNodes().item(j).hasAttributes()) {
-                        node.getChildNodes().item(i).removeChild(node.getChildNodes().item(i).getChildNodes().item(j));
-                    }
-                }
-            }
-        }
-        return node;
-    }
-
+    //method to get CNC12 version from cnc*/cnc*.prm.xml
     public static String getVersion(String directory) {
         NodeList softwareVersionNodeList;
         String softwareVersion;
         String[] softwareVersionSplit = null;
         try {
-            softwareVersionNodeList = getRootElement(getDocument("C:/" + directory + "/" + getFileDirectory(directory) + ".prm.xml")).getElementsByTagName("SoftwareVersion");
+            softwareVersionNodeList = getDocument("C:/" + directory + "/" + getFileDirectory(directory) + ".prm.xml").getDocumentElement().getElementsByTagName("SoftwareVersion");
             softwareVersion = softwareVersionNodeList.item(0).getTextContent();
             softwareVersionSplit = softwareVersion.split(" ");
         } catch (Exception e) {
@@ -200,109 +172,68 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        //define everything we need for the GUI
         Group root = new Group();
         Scene scene = new Scene(root, Color.BLACK);
-
-        stage.setTitle("Directory Renamer");
         Image icon = new Image("LK_logo.png");
+        Button cncmButton = new Button("cncm");
+        Text cncmText = new Text();
+        Button cnctButton = new Button("cnct");
+        Text cnctText = new Text();
+        Button cncrButton = new Button("cncr");
+        Text cncrText = new Text();
+        Button cncpButton = new Button("cncp");
+        Text cncpText = new Text();
+        Button cnclButton = new Button("cncl");
+        Text cnclText = new Text();
+        Button allButton = new Button("All");
+        Text allText = new Text();
+        Button[] buttons = {allButton, cncmButton, cnctButton, cncrButton, cncpButton, cnclButton};
+        Text[] texts = {allText, cncmText, cnctText, cncrText, cncpText, cnclText};
+
+        //set basic stage settings
+        stage.setTitle("Directory Renamer");
         stage.getIcons().add(icon);
-        stage.setWidth(500);
+        stage.setWidth(750);
         stage.setHeight(225);
         stage.setResizable(false);
         stage.setX(50);
         stage.setY(50);
         stage.setFullScreen(false);
 
-        Button cncmButton = new Button("cncm");
-        Text cncmText = new Text();
-        cncmText.setX(60);
-        cncmText.setY(20);
-        cncmText.setFill(Color.WHITE);
-        cncmButton.setPrefSize(50, 25);
-        cncmButton.setLayoutX(5);
-        cncmButton.setLayoutY(5);
-        cncmButton.setOnAction(event -> {
-            cncmText.setText(renamedDirectory("cncm"));
-            renameDirectory("cncm");
-            cncmButton.setText("DONE");
-        });
-        Button cnctButton = new Button("cnct");
-        Text cnctText = new Text();
-        cnctText.setX(60);
-        cnctText.setY(50);
-        cnctText.setFill(Color.WHITE);
-        cnctButton.setPrefSize(50, 25);
-        cnctButton.setLayoutX(5);
-        cnctButton.setLayoutY(35);
-        cnctButton.setOnAction(event -> {
-            cnctText.setText(renamedDirectory("cnct"));
-            renameDirectory("cnct");
-            cnctButton.setText("DONE");
-        });
-        Button cncrButton = new Button("cncr");
-        Text cncrText = new Text();
-        cncrText.setX(60);
-        cncrText.setY(80);
-        cncrText.setFill(Color.WHITE);
-        cncrButton.setPrefSize(50, 25);
-        cncrButton.setLayoutX(5);
-        cncrButton.setLayoutY(65);
-        cncrButton.setOnAction(event -> {
-            cncrText.setText(renamedDirectory("cncr"));
-            renameDirectory("cncr");
-            cncrButton.setText("DONE");
-        });
-        Button cncpButton = new Button("cncp");
-        Text cncpText = new Text();
-        cncpText.setX(60);
-        cncpText.setY(110);
-        cncpText.setFill(Color.WHITE);
-        cncpButton.setPrefSize(50, 25);
-        cncpButton.setLayoutX(5);
-        cncpButton.setLayoutY(95);
-        cncpButton.setOnAction(event -> {
-            cncpText.setText(renamedDirectory("cncp"));
-            renameDirectory("cncp");
-            cncpButton.setText("DONE");
-        });
-        Button cnclButton = new Button("cncl");
-        Text cnclText = new Text();
-        cnclText.setX(60);
-        cnclText.setY(140);
-        cnclText.setFill(Color.WHITE);
-        cnclButton.setPrefSize(50, 25);
-        cnclButton.setLayoutX(5);
-        cnclButton.setLayoutY(125);
-        cnclButton.setOnAction(event -> {
-            cnclText.setText(renamedDirectory("cncl"));
-            renameDirectory("cncl");
-            cnclButton.setText("DONE");
-        });
-        Button allButton = new Button("All");
-        allButton.setPrefSize(50, 25);
-        allButton.setLayoutX(5);
-        allButton.setLayoutY(155);
-        allButton.setOnAction(event -> {
-            cncmText.setText(renamedDirectory("cncm"));
-            cnctText.setText(renamedDirectory("cnct"));
-            cncrText.setText(renamedDirectory("cncr"));
-            cncpText.setText(renamedDirectory("cncp"));
-            cnclText.setText(renamedDirectory("cncl"));
-            renameAllDirectory();
-            allButton.setText("DONE");
-        });
-        root.getChildren().add(allButton);
-        root.getChildren().add(cncmButton);
-        root.getChildren().add(cnctButton);
-        root.getChildren().add(cncrButton);
-        root.getChildren().add(cncpButton);
-        root.getChildren().add(cnclButton);
-        root.getChildren().add(cncmText);
-        root.getChildren().add(cnctText);
-        root.getChildren().add(cncrText);
-        root.getChildren().add(cncpText);
-        root.getChildren().add(cnclText);
+        //set button and text settings and add them to the root group
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].setPrefSize(50, 25);
+            buttons[i].setLayoutX(5);
+            buttons[i].setLayoutY(5 + (i*30));
 
+            texts[i].setX(60);
+            texts[i].setY(25 + (i*30));
+            texts[i].setFill(Color.WHITE);
+            texts[i].setFont(Font.font("Courier New", FontWeight.BOLD, 20));
+
+            root.getChildren().add(buttons[i]);
+            root.getChildren().add(texts[i]);
+        }
+
+        //set actions for buttons
+        for (int i = 0; i < directories.length; i++) {
+            int j = i;
+            buttons[i + 1].setOnAction(event -> {
+            texts[j + 1].setText("DONE: " + renamedDirectory(directories[j]));
+            renameDirectory(directories[j]);
+        });}
+
+        //set action for set all button
+        allButton.setOnAction(event -> {
+            for (int i = 0; i < directories.length; i++) {
+                texts[i + 1].setText(renamedDirectory(directories[i]));
+            }
+            renameAllDirectory();
+            allText.setText("DONE");
+        });
+
+        //set the scene and show the stage
         stage.setScene(scene);
         stage.show();
     }
